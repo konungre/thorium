b280d9d9bef691e457d2fc317aca3902edce3a49
konungre <mr.prof.dr.awesome@gmail.com>
2025-08-07 11:48:42 -0500

    Update prereq-before-reboot.sh

--- minithor/prereq-before-reboot.sh
+++ minithor/prereq-before-reboot.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,7 +15,7 @@ echo \
 sudo apt-get update
 
 # Install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Manage docker as non-root user
 sudo groupadd docker
