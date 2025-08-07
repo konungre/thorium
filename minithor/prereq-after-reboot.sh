5c5e67d37cc61294fbc6d620cba83d61dd41e85a
konungre <mr.prof.dr.awesome@gmail.com>
2025-08-07 11:47:25 -0500

    Update prereq-after-reboot.sh

--- minithor/prereq-after-reboot.sh
+++ minithor/prereq-after-reboot.sh
@@ -4,9 +4,6 @@
 sudo snap install helix --classic
 sudo snap install firefox
 
-# Clone thorium
-git clone https://github.com/konungre/thorium.git
-
 # Docker login
 docker login
 cp ~/.docker/config.json ~/thorium/minithor/.dockerconfigjson
